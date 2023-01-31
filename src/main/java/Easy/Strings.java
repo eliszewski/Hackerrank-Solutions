80abe3e7ea83922da3ecdc8d87e3f63f9c4167f4
eliszewski <liszewski31098@gmail.com>
2023-01-31 15:58:19 -0500

    optimize pangrams

--- src/main/java/Easy/Strings.java
+++ src/main/java/Easy/Strings.java
@@ -1,19 +1,17 @@
 package Easy;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Strings {
     /*
     * Determine if String in a pangram
-    *
+    * O(n) time complexity
     * */
     public static String pangrams(String s){
         String alpha = "abcdefghijklmnopqrstuvwxyz";
-        Set<String> stringSet = new HashSet<String>();
-        for (int i = 0; i < s.length(); i++) {
-            stringSet.add(String.valueOf(s.charAt(i)).toLowerCase());
-        }
+        Set<String> stringSet = new HashSet<>(Arrays.asList(alpha.split("")));
         for (int i = 0; i < alpha.length(); i++) {
             if(!stringSet.contains(String.valueOf(alpha.charAt(i)))) return "not pangram";
         }

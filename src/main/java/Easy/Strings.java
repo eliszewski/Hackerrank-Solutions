7414863a718f308917aa7275166490a57da17290
eliszewski <liszewski31098@gmail.com>
2023-01-31 16:04:46 -0500

    fixed

--- src/main/java/Easy/Strings.java
+++ src/main/java/Easy/Strings.java
@@ -1,21 +1,27 @@
 package Easy;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Strings {
     /*
     * Determine if String in a pangram
-    * O(n) time complexity
+    * O(n + 26) time complexity
     * */
     public static String pangrams(String s){
         String alpha = "abcdefghijklmnopqrstuvwxyz";
-        Set<String> stringSet = new HashSet<>(Arrays.asList(alpha.split("")));
+        Set<String> stringSet = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (alpha.indexOf(Character.toLowerCase(c)) >= 0) {
+                stringSet.add(String.valueOf(c).toLowerCase());
+            }
+        }
         for (int i = 0; i < alpha.length(); i++) {
             if(!stringSet.contains(String.valueOf(alpha.charAt(i)))) return "not pangram";
         }
         return "pangram";
     }
+
 
 }
